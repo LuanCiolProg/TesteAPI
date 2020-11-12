aae091427c0a18195573c57c56603eb9c4773b05
luan ciol <69855903+LuanCiolProg@users.noreply.github.com>
2020-11-12 09:25:47 -0300

    Teste de API com Swagger

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using testeef.Data;
+using testeef.Models;
+
+namespace testeef.Controllers
+{
+
+    [ApiController]
+    [Route("v1/categories")]
+
+    public class CategoryController : ControllerBase
+    {
+        private DataContext _context;
+            
+        public CategoryController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Category>>> Get([FromServices] DataContext context)
+        {
+                var categories = await context.Categories.ToListAsync();
+                return categories;
+        }
+
+        [HttpPost]
+        [Route("")]
+
+        public async Task<ActionResult<Category>> Post(
+            [FromServices] DataContext context,
+            [FromBody]Category model)
+        { 
+            if (ModelState.IsValid)
+            {
+                context.Categories.Add(model);
+                await context.SaveChangesAsync();
+                return model;
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
---
+++ Properties/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using testeef.Data;
+
+namespace testeef.Properties
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        
+        //This method gets called by the runtime. Use this method to add services
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+            services.AddScoped<DataContext, DataContext>();
+            services.AddControllers();
+            services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version="v1",
+                    Title="AshProgHelp API",
+                    Description="Example of ASP .NET Core Web API"
+                });
+            });
+        }
+
+        //This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "AshProgHelp Test API V1");
+                c.RoutePrefix = string.Empty;
+            });
+            if (env.IsDevelopment())
+            {
+              app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
